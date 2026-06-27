74ea1a24318e6d5c8ede4912e40a4628d2057836
Lilly <128197764+Lillinator@users.noreply.github.com>
2026-06-27 14:51:39 -0700

    Improve topic author avatar rendering logic
    Refactor topic author rendering logic to check mobile layout and settings more efficiently.

--- javascripts/discourse/api-initializers/topic-list-authors-posters-init.js
+++ javascripts/discourse/api-initializers/topic-list-authors-posters-init.js
@@ -5,10 +5,17 @@ import CustomPostersHeader from "../components/custom-posters-header";
 import CustomPostersCell from "../components/custom-posters-cell";
 import CustomCategoriesLatestAvatar from "../components/custom-categories-latest-avatar";
 
-// Show topic author avatar on categories page if setting enabled
 export default apiInitializer((api) => {
-  if (settings.show_author_on_categories_page === true) {
+  // Look up the site service to check the current layout
+  const site = api.container.lookup("service:site");
+  
+  // Bail out early if the setting is true and we are rendering the mobile layout
+  if (settings.disable_on_mobile && site.mobileView) {
+    return;
+  }
 
+  // Show topic author avatar on categories page if setting enabled
+  if (settings.show_author_on_categories_page) {
     api.renderInOutlet(
       "latest-topic-list-item-topic-poster",
       CustomCategoriesLatestAvatar
@@ -27,7 +34,7 @@ export default apiInitializer((api) => {
     );
 
     // Replace posters header AND poster column contents in topic lists if setting enabled 
-    if (settings.topic_list_show_last_poster_only === true) {
+    if (settings.topic_list_show_last_poster_only) {
       columns.replace("posters", { 
         item: CustomPostersCell, 
         header: CustomPostersHeader
@@ -37,6 +44,6 @@ export default apiInitializer((api) => {
     return columns;
   });
 
-    // Add topic author to Mobile view
-   api.renderInOutlet("topic-list-item-mobile-avatar", AuthorColumnContent);
+  // Add topic author to Mobile view
+  api.renderInOutlet("topic-list-item-mobile-avatar", AuthorColumnContent);
 });
